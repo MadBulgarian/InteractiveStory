302b9344cdf20e1deabb56f19fb1b063092570fa
Ivan Marinov <ivanpb7@yahoo.com>
2018-02-23 23:45:37 -0700

    Initial commit

---
+++ app/src/main/java/com/idmarinov/interactivestory/model/Choice.java
@@ -0,0 +1,31 @@
+package com.idmarinov.interactivestory.model;
+
+/**
+ * Created by Ivan on 2/23/2018.
+ */
+
+public class Choice {
+    private int textId;
+    private int nextPage;
+
+    public Choice(int textId, int nextPage) {
+        this.textId = textId;
+        this.nextPage = nextPage;
+    }
+
+    public int getTextId() {
+        return textId;
+    }
+
+    public void setTextId(int textId) {
+        this.textId = textId;
+    }
+
+    public int getNextPage() {
+        return nextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        this.nextPage = nextPage;
+    }
+}
